82d7a71fa3aa35a9a50ed6b7b70f813c3b794757
Kevin McPherson <kjmcpherson@gmail.com>
2018-01-10 01:35:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+board      = ["x", "x", "x", "x", "x", "x", "x"]
 def display_board(board)
-  puts "   |   |   "
+  puts "#{0}|   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
@@ -10,3 +11,15 @@ def display_board(board)
 end
 
 display_board(board)
+
+
+colors     = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+
+end
+
+display_rainbow(colors)
+
+#outputs -- R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet
