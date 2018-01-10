85ebfbf5dae60af78f04befa754095a50bf1a2a5
Kevin McPherson <kjmcpherson@gmail.com>
2018-01-10 01:15:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,10 @@
 # out the current state.
 
 def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
