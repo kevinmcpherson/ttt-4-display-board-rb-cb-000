036067be9c367985bc3f027f028a5fb7ddc4cc2d
Kevin McPherson <kjmcpherson@gmail.com>
2018-01-10 01:37:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board      = ["x", "x", "x", "x", "x", "x", "x"]
 def display_board(board)
-  puts "#{0}|   |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
